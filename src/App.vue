478394f1cd57ac8dddd9ef45d6ea8e0c51f72f62
yuqing521 <1581446178@qq.com>
2021-12-16 10:53:30 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <!-- <router-link to="/">首页</router-link>
+        <router-link to="/pins">沸点</router-link>
+        <router-link to="/news">资讯</router-link>
+        <router-link to="/course">课程</router-link>
+        <router-link to="/events/all">活动</router-link> -->
+        <HeaderComponentVue/>
+      </nav>
+    </div>
+  </header>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+  import HeaderComponentVue from '@components/HeaderComponent/index.vue';
+</script>
+
